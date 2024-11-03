0939369a84f08622a02aaaefcbe12fa0e5555941
Nitesh Sharma <140998787+NiteshVLSI@users.noreply.github.com>
2024-11-03 10:56:20 +0530

    Add files via upload

--- valid.v
+++ valid.v
@@ -1,3 +1,4 @@
+
 module is_valid(fp,x,y,exponent,clk,reset,valid);
 input wire [31:0] fp;
 input wire clk,reset;
@@ -19,9 +20,9 @@ begin
         begin
         if((fp[31]==0)&&(fp[22:0]!=23'b0))
             begin
-            x <= {(7'b0000010),fp[21:0],3'b0};
-            y <= {7'b0,fp[21:0],3'b0};
-            exponent <= fp[30:23];
+            x <= {(7'b0000010),fp[22:0],2'b0};
+            y <= {7'b0,fp[22:0],2'b0};
+            exponent <= (fp[30:23]>7'd127)?(fp[30:23]-7'd127):({1'b1,7'd127-fp[29:23]});
             valid <= 1'b1;
             end
         else
